090e2985c8d8d016043ab96221456fa47d92d508
Pratik Nagdeve <11483594+tadiparChinese@users.noreply.github.com>
2021-11-29 14:14:47 +0530

    Update 36_25_more_ranges.py

--- 36_25_more_ranges.py
+++ 36_25_more_ranges.py
@@ -3,7 +3,7 @@
 str = 'abcdefghijklmnopqrstuvwxyz'
 
 #print every second character
-print(str[::2])
+print(str[::2]) #step function
 
 #print every third character
-print(str[::3])
+print(str[::3]) #step function
